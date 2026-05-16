e872315dbbc3fdbd6ff16f09a5589e8738adb561
Andres Fernandez <andresfer179@gmail.com>
2026-05-16 13:08:37 -0300

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Cuando en local busques '/deezer', Vite lo va a redirigir a la API real
+      '/deezer': {
+        target: 'https://api.deezer.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/deezer/, ''), // Le quita el '/deezer' antes de mandarlo
+      },
+    },
+  },
 })
